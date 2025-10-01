1b80097e43570653d62593825a04e7de1acb83f9
kashinath mali <kashinathmali88@gmail.com>
2025-10-01 12:32:56 +0530

    db change

--- server/db.js
+++ server/db.js
@@ -1,14 +1,23 @@
 import mongoose from "mongoose";
 
-export const connectDB = () => {
-  mongoose
-    .connect(`${process.env.MONGO_URI}/ClerkTest`)
-    .then(() => {
-      console.log("connectd to db");
-    })
-    .catch((err) => {
-      console.log(err);
+// export const connectDB = () => {
+//   mongoose
+//     .connect(`${process.env.MONGO_URI}/ClerkTest`)
+//     .then(() => {
+//       console.log("connectd to db");
+//     })
+//     .catch((err) => {
+//       console.log(err);
 
-      console.log("failed to connect");
-    });
+//       console.log("failed to connect");
+//     });
+// };
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/ClerkTest`);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log("Failed to connect", error);
+  }
 };
